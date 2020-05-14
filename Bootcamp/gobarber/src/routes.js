a31280ffcb171e777d3b562ded90a944093697f7
Caio Telles <cast@icomp.ufam.edu.br>
2020-05-14 14:33:07 -0400

    Create/update user implemented in node

--- Bootcamp/gobarber/src/routes.js
+++ Bootcamp/gobarber/src/routes.js
@@ -1,15 +1,22 @@
 import { Router } from 'express';
-import User from './app/models/user';
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Caio Arthur',
-    email: 'caio.telles@itbam.org.br',
-    password_hash: '12345678',
-  });
-  res.json(user);
-});
+routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
+routes.use(authMiddleware);
+routes.put('/users', UserController.update);
+
+// routes.get('/', async (req, res) => {
+//   const user = await User.create({
+//     name: 'Fulano',
+//     email: 'fulano@itbam.org.br',
+//     password_hash: '12345678',
+//   });
+//   res.json(user);
+// });
 
 export default routes;
